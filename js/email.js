e998926a9870e1cbb707e4e7e486055a69d49a7e
Miguel Aleixo <miguelgomes2099@hotmail.com>
2017-06-09 15:13:39 -0300

    CandyShop

--- js/email.js
+++ js/email.js
@@ -1,16 +1,10 @@
 window.onload = function inicio() {
     document.getElementById("email").addEventListener("blur", function () {
-        var input = document.getElementsByTagName("input").value;
-        if (input !== null ) {
-            document.getElementsByTagName("label")[0].style.fontSize = "12px";
-            document.getElementsByTagName("label")[0].style.top = "-20px";
-            document.getElementsByTagName("label")[0].style.transition = "0.2s";
-        } else if(input === null) {
-            document.getElementsByTagName("label")[0].style.fontSize = "8pt";
-            document.getElementsByTagName("label")[0].style.top = "-10px";
-            document.getElementsByTagName("label")[0].style.transition = "0.2s";
-            document.getElementsByTagName("label")[0].style.color = "#e91e63";
+        var input = document.getElementById("email").value;
+        if (input){
+            document.getElementsByTagName("label")[0].classList.add("uso");
+        } else{
+            document.getElementsByTagName("label")[0].classList.remove("uso");
         }
-        ;
     });
 }
--- js/senha.js
+++ js/senha.js
@@ -2,16 +2,9 @@ window.onload = function inicio() {
     document.getElementById("senha").addEventListener("blur", function () {
         var input = document.getElementById("senha").value;
         if (input){
-            document.getElementsByTagName("label")[0].style.fontSize = "12px";
-            document.getElementsByTagName("label")[0].style.top = "-20px";
-            document.getElementsByTagName("label")[0].style.transition = "0.2s";
+            document.getElementsByTagName("label")[0].classList.add("uso");
         } else{
-            document.getElementsByTagName("label")[0].style.color = "rgba(0,0,0,0.42)";
-            document.getElementsByTagName("label")[0].style.top = "3px";
-            document.getElementsByTagName("label")[0].style.fontSize = "16px";
-            document.getElementsByTagName("label")[0].style.transition = "0.2s";
-         }
-
-
+            document.getElementsByTagName("label")[0].classList.remove("uso");
+        }
     });
 }
