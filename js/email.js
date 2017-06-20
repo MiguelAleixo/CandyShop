166171c1dbff467776ce5bb64783e674e85dced1
Miguel Aleixo <miguelgomes2099@hotmail.com>
2017-06-20 09:48:11 -0300

    CandyShop

--- js/email.js
+++ js/email.js
@@ -1,10 +1,19 @@
 window.onload = function inicio() {
-    document.getElementById("email").addEventListener("blur", function () {
-        var input = document.getElementById("email").value;
-        if (input){
-            document.getElementsByTagName("label")[0].classList.add("uso");
-        } else{
-            document.getElementsByTagName("label")[0].classList.remove("uso");
-        }
+    document.getElementById("email").addEventListener("focus", function () {
+        document.getElementsByTagName("label")[0].classList.add("sobelabel");
+        document.getElementsByTagName("label")[0].classList.remove("cinza");
     });
-}
+    document.getElementById("email").addEventListener("blur", function (){
+        document.getElementsByTagName("label")[0].classList.remove("sobelabel");
+        var input = document.getElementById("email").value;
+        if (input) {
+            document.getElementsByTagName("label")[0].classList.add("cinza");
+            document.getElementsByTagName("label")[0].classList.add("sobelabel");
+        }
+        else if (!input){
+            document.getElementsByTagName("label")[0].classList.remove("cor");
+            document.getElementsByTagName("label")[0].classList.remove("foco");
+        }
+
+    });
+};
--- js/senha.js
+++ js/senha.js
@@ -1,10 +1,61 @@
 window.onload = function inicio() {
-    document.getElementById("senha").addEventListener("blur", function () {
-        var input = document.getElementById("senha").value;
-        if (input){
-            document.getElementsByTagName("label")[0].classList.add("uso");
-        } else{
-            document.getElementsByTagName("label")[0].classList.remove("uso");
+    document.getElementById("senha").addEventListener("focus", function () {
+        document.getElementsByTagName("label")[0].classList.add("sobelabel");
+        document.getElementsByTagName("label")[0].classList.add("vermelho");
+        document.getElementsByTagName("p")[0].classList.add("si");
+        document.getElementsByTagName("p")[1].classList.remove("hp");
+        var valid = document.getElementById("senha").value.length;
+        if (valid >= 6) {
+            document.getElementsByTagName("label")[0].classList.add("rosa");
+            document.getElementsByTagName("label")[0].classList.remove("vermelho");
+            document.getElementsByTagName("p")[0].classList.remove("si");
+
+        }
+        else{
+            document.getElementsByTagName("label")[0].classList.remove("rosa");
+            document.getElementsByTagName("label")[0].classList.add("vermelho");
         }
     });
-}
+    document.getElementById("senha").addEventListener("blur", function () {
+
+        var input = document.getElementById("senha").value.length;
+        if (input > 0 && input < 6) {
+            document.getElementsByTagName("label")[0].classList.add("sobelabel");
+            document.getElementsByTagName("label")[0].classList.add("vermelho");
+        }
+        else if (input >= 6) {
+            document.getElementsByTagName("label")[0].classList.add("sobelabel");
+            document.getElementsByTagName("label")[0].classList.add("cinza");
+            document.getElementsByTagName("label")[0].classList.remove("vermelho");
+            document.getElementsByTagName("label")[0].classList.remove("rosa");
+    }
+        else {
+            document.getElementsByTagName("label")[0].classList.remove("sobelabel");
+        }
+        var barra = document.getElementById("senha").value.length;
+            if (barra > 0 && barra < 6){
+                document.getElementsByTagName("input")[0].classList.add("barred");
+            }
+            else if (barra >= 6) {
+                document.getElementsByTagName("input")[0].classList.remove("barred");
+            }
+            else {
+                document.getElementsByTagName("input")[0].classList.add("barred");
+            }
+    });
+    document.getElementById("senha").addEventListener("keydown", function () {
+        setTimeout(function () {
+            var valid = document.getElementById("senha").value.length;
+            if (valid >= 6) {
+                document.getElementsByTagName("label")[0].classList.add("rosa");
+                document.getElementsByTagName("label")[0].classList.remove("vermelho");
+                document.getElementsByTagName("p")[0].classList.remove("si");
+            }
+            else{
+                document.getElementsByTagName("label")[0].classList.remove("rosa");
+                document.getElementsByTagName("label")[0].classList.add("vermelho");
+                document.getElementsByTagName("p")[0].classList.add("si");
+            }
+        });
+    });
+};
