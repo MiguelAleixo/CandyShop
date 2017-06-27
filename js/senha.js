0b4e5e9010637a967fb7710292dd1259168acb97
Miguel Aleixo <miguelgomes2099@hotmail.com>
2017-06-27 08:59:41 -0300

    att

--- js/senha.js
+++ js/senha.js
@@ -8,37 +8,37 @@ window.onload = function inicio() {
             document.getElementsByTagName("label")[0].classList.add("rosa");
             document.getElementsByTagName("label")[0].classList.remove("vermelho");
             document.getElementsByTagName("p")[0].classList.remove("si");
+            document.getElementsByTagName("button")[0].removeAttribute("disabled");
         }
         else{
             document.getElementsByTagName("label")[0].classList.remove("rosa");
             document.getElementsByTagName("label")[0].classList.add("vermelho");
+            document.getElementsByTagName("button")[0].setAttribute("disabled","disabled");
         }
     });
     document.getElementById("senha").addEventListener("blur", function () {
-
         var input = document.getElementById("senha").value.length;
         if (input > 0 && input < 6) {
-            document.getElementsByTagName("label")[0].classList.add("sobelabel");
+            // document.getElementsByTagName("label")[0].classList.add("sobelabel");
             document.getElementsByTagName("label")[0].classList.add("vermelho");
+            document.getElementsByTagName("button")[0].setAttribute("disabled","disabled");
         }
         else if (input >= 6) {
-            document.getElementsByTagName("label")[0].classList.add("sobelabel");
+            // document.getElementsByTagName("label")[0].classList.add("sobelabel");
             document.getElementsByTagName("label")[0].classList.add("cinza");
             document.getElementsByTagName("label")[0].classList.remove("vermelho");
             document.getElementsByTagName("label")[0].classList.remove("rosa");
+            document.getElementsByTagName("button")[0].removeAttribute("disabled");
     }
         else {
             document.getElementsByTagName("label")[0].classList.remove("sobelabel");
         }
         var barra = document.getElementById("senha").value.length;
-if (barra > 0 && barra < 6){
+        if (barra < 6){
     document.getElementsByTagName("input")[0].classList.add("barred");
 }
-else if (barra >= 6) {
+    else if (barra >= 6) {
     document.getElementsByTagName("input")[0].classList.remove("barred");
-}
-else {
-    document.getElementsByTagName("input")[0].classList.add("barred");
 }
 });
 document.getElementById("senha").addEventListener("keydown", function () {
@@ -50,6 +50,7 @@ document.getElementById("senha").addEventListener("keydown", function () {
             document.getElementsByTagName("p")[0].classList.remove("si");
             document.getElementsByTagName("button")[0].classList.add("botao");
             document.getElementsByTagName("button")[0].classList.remove("botaop");
+            document.getElementsByTagName("button")[0].removeAttribute("disabled");
         }
         else{
             document.getElementsByTagName("label")[0].classList.remove("rosa");
@@ -57,6 +58,7 @@ document.getElementById("senha").addEventListener("keydown", function () {
             document.getElementsByTagName("p")[0].classList.add("si");
             document.getElementsByTagName("button")[0].classList.remove("botao");
             document.getElementsByTagName("button")[0].classList.add("botaop");
+            document.getElementsByTagName("button")[0].setAttribute("disabled","disabled");
         }
     });
 });
